65b7f2ad75b1aa52a24e646869aca13a96f68a06
koop4 <dilillo.roberto@gmail.com>
2017-07-29 12:31:47 +0200

    fade in and out via jquery

--- js/quotesFetcher.js
+++ js/quotesFetcher.js
@@ -1,26 +1,34 @@
-window.onload = loadQuotes();
+
+$(document).ready(loadQuotes);
 
 var quotes = [];
-
+var container;
+var quoteTag;
+var writerTag;
 
 function loadQuotes(){
-  var header = new Headers({
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'multipart/form-data'
-  });
+  container = $("#quote-container");
+  quoteTag = container.find("p")[0];
+  writerTag = container.find("cite")[0];
+  console.log(quoteTag)
+  var header = new Headers({ 'Access-Control-Allow-Origin': '*', 'Content-Type': 'multipart/form-data' });
   fetch('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30', {mode: 'cors', header: header})  
     .then(function(response) { return response.json(); })
-    .then(function(response) { quotes = response; })
+    .then(function(response) { quotes = response; return 0; })
     .then(newQuotes)
     .catch(function(err) { console.log('Fetch Error', err); });;
 }
 
-function newQuotes(){
-    var container = document.getElementById('quote-container');
+
+
+
+function newQuotes(time){
     var randomIndex = Math.floor(Math.random() * quotes.length);
     var randomQuote = quotes[randomIndex]
-    container.innerHTML = randomQuote.content;
-    var cite = document.createElement("cite");
-    cite.innerHTML = randomQuote.title;
-    container.prepend(cite);
+    container.fadeOut(time, changeQuote);
+    function changeQuote() {
+      quoteTag.innerHTML = randomQuote.content;
+      writerTag.innerHTML = randomQuote.title;
+      container.fadeIn(1000);
+    }
 }
