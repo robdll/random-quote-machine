abb9aec546d6a03d5c8c5b081f432ba6c7b648f9
koop4 <dilillo.roberto@gmail.com>
2017-07-30 18:48:13 +0200

    remove log

--- js/quotesManager.js
+++ js/quotesManager.js
@@ -11,7 +11,6 @@ function loadQuotes() {
   container = $("#quote-container");
   quoteTag = container.find("p")[0];
   writerTag = container.find("cite")[0];
-  console.log(quoteTag)
   var header = new Headers({ 'Access-Control-Allow-Origin': '*', 'Content-Type': 'multipart/form-data' });
   fetch('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30', {mode: 'cors', header: header})  
     .then(function(response) { return response.json(); })
