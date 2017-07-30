41b2d3d64deb3d24ab71d199e10bf74f9fc9e0bb
koop4 <dilillo.roberto@gmail.com>
2017-07-30 19:21:30 +0200

    add static data in case api fails

--- js/quotesManager.js
+++ js/quotesManager.js
@@ -16,7 +16,10 @@ function loadQuotes() {
     .then(function(response) { return response.json(); })
     .then(function(response) { quotes = response; return 0; })
     .then(newQuotes)
-    .catch(function(err) { newQuotes(0); });
+    .catch(function(err) { 
+      newQuotes(0); 
+      addStaticQuotes();
+    });
 }
 
 function newQuotes(time){
@@ -29,6 +32,23 @@ function newQuotes(time){
     writerTag.innerHTML = randomQuote.title;
     container.fadeIn(1000);
   }
+}
+
+function addStaticQuotes(){
+  quotes = [
+    { content: "Thousands of candles can be lighted from a single candle, and the life of the candle will not be shortened. Happiness never decreases by being shared." , title: "Buddha" },
+    { content: "Success is going from failure to failure without losing enthusiasm." , title: "Winston Churchill" },
+    { content: "The way to love anything is to realize that it may be lost." , title: "Gilbert K. Chesterton" },
+    { content: "If one dream should fall and break into a thousand pieces, never be afraid to pick one of those pieces up and begin again" , title: "Flavia Weedn" },
+    { content: "You get treated in life the way you teach people to treat you." , title: "Wayne Dyer" },
+    { content: "Life is very interesting. In the end, some of your greatest pains become your greatest strengths" , title: "Drew Barrymore" },
+    { content: "If you always do what you’ve always done, you’ll always get what you always got." , title: "Mark Twain" },
+    { content: "Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world." , title: "Lucille Ball" },
+    { content: "It takes courage to love, but pain through love is the purifying fire which those who love generously know. We all know people who are so much afraid of pain that they shut themselves up like clams in a shell and, giving out nothing, receive nothing and therefore shrink until life is a mere living death" , title: "Eleanor Roosevelt" },
+    { content: "Arguing with a fool proves there are two" , title: "Doris M. Smith" },
+    { content: "Wealth is the ability to fully experience life." , title: "Henry David Thoreau" },
+    { content: "Besides the noble art of getting things done, there is the noble art of leaving things undone. The wisdom of life consists in the elimination of non-essentials." , title: "Lin Yutang" },
+  ];
 }
 
 function tweet() {
