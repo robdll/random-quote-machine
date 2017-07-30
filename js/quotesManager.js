0fc47eecdb0c444ae772d76569ef045b3578d41c
koop4 <dilillo.roberto@gmail.com>
2017-07-30 18:23:34 +0200

    add feature to tweet the quote

--- js/quotesManager.js
+++ js/quotesManager.js
@@ -5,8 +5,9 @@ var quotes = quotes= [{ content: 'Sometimes API fetch goes wrong and you\ are le
 var container;
 var quoteTag;
 var writerTag;
+var currentQuote;
 
-function loadQuotes(){
+function loadQuotes() {
   container = $("#quote-container");
   quoteTag = container.find("p")[0];
   writerTag = container.find("cite")[0];
@@ -19,17 +20,20 @@ function loadQuotes(){
     .catch(function(err) { newQuotes(0); });
 }
 
-
-
-
 function newQuotes(time){
-    var randomIndex = Math.floor(Math.random() * quotes.length);
-    var randomQuote = quotes[randomIndex]
-    container.fadeOut(time, changeQuote);
-    function changeQuote() {
-      var text = randomQuote.content.replace(/<\/?p>/g,'').replace(/\. /g,'.<br>').replace(/—/g,' ');
-      quoteTag.innerHTML = text;
-      writerTag.innerHTML = randomQuote.title;
-      container.fadeIn(1000);
-    }
+  var randomIndex = Math.floor(Math.random() * quotes.length);
+  var randomQuote = quotes[randomIndex]
+  container.fadeOut(time, changeQuote);
+  function changeQuote() {
+    currentQuote = randomQuote.content.replace(/<\/?p>/g,'').replace(/\. /g,'.<br>').replace(/—/g,' ');
+    quoteTag.innerHTML = currentQuote;
+    writerTag.innerHTML = randomQuote.title;
+    container.fadeIn(1000);
+  }
+}
+
+function tweet() {
+  var y = window.top.outerHeight / 2 + window.top.screenY - ( 400 / 2);
+  var x = window.top.outerWidth / 2 + window.top.screenX - ( 550 / 2);
+  window.open("https://twitter.com/intent/tweet?text="+currentQuote.replace(/ /g,'%20'), "_blank", "width=550, height=400, top="+y+", left="+x);
 }
