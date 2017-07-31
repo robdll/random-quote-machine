cd95df955072dfa2df1cd0b4debb96c7dab2cd1f
koop4 <dilillo.roberto@gmail.com>
2017-07-31 07:32:12 +0200

    also tweet artist name

--- js/quotesManager.js
+++ js/quotesManager.js
@@ -27,9 +27,12 @@ function newQuotes(time){
   var randomQuote = quotes[randomIndex]
   container.fadeOut(time, changeQuote);
   function changeQuote() {
-    currentQuote = randomQuote.content.replace(/<\/?p>/g,'').replace(/\. /g,'.<br>').replace(/—/g,' ');
-    quoteTag.innerHTML = currentQuote;
-    writerTag.innerHTML = randomQuote.title;
+    currentQuote = {
+      text: randomQuote.content.replace(/<\/?p>/g,'').replace(/\. /g,'.<br>').replace(/—/g,' '),
+      artist: randomQuote.title
+    }
+    quoteTag.innerHTML = currentQuote.text;
+    writerTag.innerHTML = currentQuote.artist;
     container.fadeIn(1000);
   }
 }
@@ -54,5 +57,6 @@ function addStaticQuotes(){
 function tweet() {
   var y = window.top.outerHeight / 2 + window.top.screenY - ( 400 / 2);
   var x = window.top.outerWidth / 2 + window.top.screenX - ( 550 / 2);
-  window.open("https://twitter.com/intent/tweet?text="+currentQuote.replace(/ /g,'%20'), "_blank", "width=550, height=400, top="+y+", left="+x);
+  var tweet = (currentQuote.text+' - '+currentQuote.artist).replace(/ /g,'%20');
+  window.open("https://twitter.com/intent/tweet?text="+tweet, "_blank", "width=550, height=400, top="+y+", left="+x);
 }
